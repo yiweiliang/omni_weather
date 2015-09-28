825ead0e384a1f68851ce3811888994b83e8c4fe
Liang <yiweiliang@MacBook-Air-de-Yiwei.local>
2015-09-28 11:32:26 -0400

    weather app
    responsive weather app assignment

---
+++ weather_query.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Response Page</title>
+	<link rel="stylesheet" type="text/css" href="weather.css"
+</head>
+<body>
+<div class="container-sun">
+<svg class="svg-sun" version="1.1" viewBox="0 0 100 100" preserveAspectRatio="xMinYMin meet">
+<circle cx="50" cy="50" r="35" id="sun"></circle>
+</svg>
+</div>
+
+<?php
+	if (isset($_GET['city']) ) {
+		$open_url = 'http://api.openweathermap.org/data/2.5/weather?';	
+		$open_id = '2b38602d8c6ac06babab303acc148081';
+		
+
+		$open_urll = $open_url .'q='. $_GET['city']. '&APPID=' . $open_id . '&units=metric';
+		$open_results = file_get_contents($open_urll);
+		$object = json_decode($open_results);
+		$temp =$object->main->temp;
+        $condition=$object->weather[0]->description;
+		  $html  = "'<div class='weather'><h3>The Weather for &nbsp</h3> " . "<h2>".$object->name . "</h2>" . " now is " . $temp . "&deg and the weather condition is: " . $condition; 
+		echo $html . "</div>"; 	
+	}
+	else{
+		echo "error";	
+	}
+	?>
+
+</body>
+</html>
+
